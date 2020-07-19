7714dffbd013e8baf44ac7def5957b978ba35297
carolkinyua-hub <carolnkinyua@gmail.com>
2020-07-19 06:17:05 +0300

    add Akan name formula

--- js/script.js
+++ js/script.js
@@ -1,24 +1,15 @@
 //Details of what the user should enter//
+var CC=parseInt( prompt("Which year were you born?:"));
+var YY=parseInt( prompt("Enter the last 2 digits of the year you were born:"));
+var MM=parseInt( prompt("Enter the month you were born:"));
+var DD=parseInt( prompt("Enter the day of the month you were born:"));
 
 
-var CC= prompt(parseInt ("Which year were you born?:"));
-var YY= prompt(parseInt ("Enter the last 2 digits of the year you were born:"));
-var MM=prompt(parseInt("Enter the month you were born:"));
-var DD=prompt(parseInt("Enter the day of the month you were born:"));
+
+
+var dow=function(cc, yy, mm, dd){
+return (((cc/4) -2*cc-1)  + ((5*yy/4))  +((26*(mm+1)/10)) +dd) % 7;
+};
 
 
 //Formula to compute the day of birth//
-
-Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
- where;
-
- CC - is the century digits. For example 1989 has CC = 19
-
- YY - is the Year digits (1989 has YY = 89)
-
- MM -  is the Month
-
- DD - is the Day of the month
-
- mod - is the modulus function ( % )
