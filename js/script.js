1b440230c476d4c420d94160292dde061074619b
carolkinyua-hub <carolnkinyua@gmail.com>
2020-07-20 08:08:26 +0300

    add a radio button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-//Details of what the user should enter//
-var CC=parseInt( prompt("Which year were you born?:"));
-var YY=parseInt( prompt("Enter the last 2 digits of the year you were born:"));
-var MM=parseInt( prompt("Enter the month you were born:"));
-var DD=parseInt( prompt("Enter the day of the month you were born:"));
-
-
-
-
-var dow=function(cc, yy, mm, dd){
+//Akan name function
+function day(){
+  var cc=document.getElementById("year"))
+  var yy=document.getElementById("year-2"))
+  var mm=document.getElementById("month"))
+  var dd=document.getElementById("date"))
+}
+var akanName=function(cc, yy, mm, dd){
 return (((cc/4) -2*cc-1)  + ((5*yy/4))  +((26*(mm+1)/10)) +dd) % 7;
 };
-
-
-//Formula to compute the day of birth//
+alert(var AkanName)
